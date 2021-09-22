4be30e2fcbd65edb7dc31acc9b73dea2f7e6bff1
Abd Wahid <wahed.blog99@gmail.com>
2021-09-23 01:09:09 +0700

    get api products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,18 +7,17 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function get(){
+    function get(){
         $product = Product::All();
         if($product){
-            
             $response = [
                 "is_success" => true,
                 "message" => "Get All Data",
                 "data" => $product,
             ];
-
             return response()->json($response);
-
+        }else{
+            return response()->json("Error");
         }
     }
 }
